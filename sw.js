fb3be87c16d2593380fdf072e8a6bcafef54922c
salomonAMS <amssalomon@gmail.com>
2025-08-07 13:01:03 +0000

    modif mobile

--- sw.js
+++ sw.js
@@ -1,26 +1,37 @@
 // Service Worker pour Portfolio - Cache optimisé
-// Version 1.0 - Stratégie Cache First pour les ressources statiques
+// Version 1.3 - Correction du menu hamburger
 
-const CACHE_NAME = 'portfolio-v1.0';
+const CACHE_NAME = 'portfolio-v1.3';
 const URLS_TO_CACHE = [
     '/',
     '/index.html',
+    '/offline.html',
     '/assets/css/meyawo.css',
-    '/assets/css/critical.css',
+    '/assets/css/critical-layout.css',
+    '/assets/css/performance-patch.css',
     '/assets/js/portfolio-app.js',
     '/assets/js/resource-optimizer.js',
     '/assets/js/mobile-responsive.js',
+    '/assets/js/performance-fixes.js',
+    '/assets/js/security-performance-fix.js',
+    '/assets/js/hamburger-fix.js',
     '/assets/js/meyawo.js',
     '/assets/js/typewriter.js',
     '/assets/js/navbar-scroll.js',
     '/assets/vendors/themify-icons/css/themify-icons.css',
+    '/assets/vendors/themify-icons/css/themify-icons-optimized.css',
+    '/assets/vendors/themify-icons/fonts/themify.woff2',
+    '/assets/vendors/themify-icons/fonts/themify.woff',
     '/assets/imgs/mon logo.png',
     '/assets/imgs/télécharger (3).jpeg',
     '/assets/imgs/man.png',
     '/assets/imgs/analytics.svg',
     '/assets/imgs/toolbox.svg',
     '/assets/imgs/responsive.svg',
-    '/assets/imgs/pencil-case.svg'
+    '/assets/imgs/pencil-case.svg',
+    '/assets/imgs/todolist.shot.png',
+    '/assets/imgs/auth.shot.png',
+    '/assets/imgs/Bot.shot.png'
 ];
 
 // Installation du Service Worker
